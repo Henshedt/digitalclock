b9b55f633f10c2e4225b7158d47f20efd7e0c777
Guilherme Fiuza <91514791+Henshedt@users.noreply.github.com>
2022-07-26 17:29:29 -0300

    Add files via upload

--- relógio_digital/assets/js/script.js
+++ relógio_digital/assets/js/script.js
@@ -5,9 +5,9 @@ const segundos = document.getElementById('segundos');
 const relogio = setInterval(function time() {
 
     let dateToday = new Date();
-    let hr = dateToday.getHours();
-    let min = dateToday.getMinutes();
-    let s = dateToday.getSeconds();
+    let hr = dateToday.getUTCHours();
+    let min = dateToday.getUTCMinutes();
+    let s = dateToday.getUTCSeconds();
 
     horas.textContent = hr;
     minutos.textContent = min;
